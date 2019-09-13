9abf6bcb3b73db2b26501935fc54e741f034f562
Jakie W. Rice <jakie.rice@gmail.com>
2019-09-13 09:25:30 -0500

    refactor(nextjs): adds next wrapper around socket server

--- server/index.js
+++ server/index.js
@@ -1,26 +1,24 @@
-var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
-var randomNumber = require('./randomNumber');
+const app = require('express')();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const randomNumber = require('../utils/randomNumber');
+const next = require('next');
 
-var port = process.env.PORT || 3000;
-
-app.get('/', function (req, res) {
-  res.send('Use socket.io-client to connect to the server...');
-});
+const dev = process.env.NODE_ENV !== 'production';
+const nextApp = next({ dev });
+const nextHandler = nextApp.getRequestHandler();
+const port = process.env.PORT || 3000;
 
 io.on('connection', function (socket) {
-  console.log('connect');
-
-  var unsubscribe = randomNumber.subscribe(function (number) {
+  const unsubscribe = randomNumber.subscribe(function (number) {
     console.log(number);
 
-    var data = {
+    const data = {
       value: number,
       timestamp: Number(new Date()),
     };
 
-    socket.emit('data', data);
+    socket.emit('now', data);
   });
 
   socket.on('disconnect', function () {
@@ -29,6 +27,13 @@ io.on('connection', function (socket) {
   });
 });
 
-http.listen(port, function () {
-  console.log('listening on *:' + port);
+nextApp.prepare().then(() => {
+  app.get('*', (req, res) => {
+    return nextHandler(req, res);
+  });
+
+  http.listen(port, err => {
+    if (err) throw err;
+    console.log(`> Listening on http://localhost:${port}`);
+  });
 });
