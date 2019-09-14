20ed875c01ed2a5e3c69b7629a321c23c7970f02
Jakie W. Rice <jakie.rice@gmail.com>
2019-09-13 22:27:29 -0500

    refactor(typescript): remove ts for now now v2 will not work with web sockts. now v1 requires a fair amount of additional config for ts. Removing for now in favor of moving quickly for the exercise

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,19 +1,23 @@
 import React from 'react';
 import io from 'socket.io-client';
 
+const URL = process.env.NODE_ENV === 'production' ? 'https://react-task.jakierice.now.sh/' : '';
+
 class Home extends React.Component {
   state = {
     hello: '',
   };
-  private socket: any;
+  static socket;
   componentDidMount() {
+    console.log(URL);
     this.socket = io();
-    this.socket.on('now', (data: any) => {
+    this.socket.on('now', (data) => {
       this.setState({
         hello: data.value,
       });
     });
   }
+
   render() {
     return <div>{this.state.hello}</div>;
   }
