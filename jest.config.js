6b83c75a5d011c4fcf73b69ef6182adb5f0b4a1b
Jakie W. Rice <jakie.rice@gmail.com>
2019-09-13 23:50:26 -0500

    chore: remove test setup config

--- jest.config.js
+++ jest.config.js
@@ -6,8 +6,4 @@ module.export = {
   testMatch: ['<rootDir>/**/>(*.)test.{js, jsx}'], // finds test
   moduleFileExtensions: ['js', 'jsx', 'json', 'node'],
   testPathIgnorePatterns: ['/node_modules/', '/.next/'],
-  setupFilesAfterEnv: [
-    'jest-dom/extend-expect',
-    '@testing-library/react/cleanup-after-each'
-  ] // setupFiles before the tests are ran
 };
