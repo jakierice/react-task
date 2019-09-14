6f84f13f6a4a5a6f25d4ed38f37e416c6cb9753a
Jakie W. Rice <jakie.rice@gmail.com>
2019-09-14 00:34:19 -0500

    refactor(testing): try next test example config

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,4 @@
-module.export = {
-  roots: ['<rootDir>/pages'],
-  transform: {
-    '\\.(js|jsx)?$': 'babel-jest',
-  },
-  testMatch: ['<rootDir>/pages/**/>(*.)test.{js, jsx}'], // finds test
-  moduleFileExtensions: ['js', 'jsx', 'json', 'node'],
-  testPathIgnorePatterns: ['/node_modules/', '/.next/'],
-};
+module.exports = {
+  // setupFiles: ['<rootDir>/jest.setup.js'],
+  testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/']
+}
