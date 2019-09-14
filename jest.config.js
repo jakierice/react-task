46bff55ba7b53c9f5139c74f679c24a9edd3e314
Jakie W. Rice <jakie.rice@gmail.com>
2019-09-14 00:08:13 -0500

    fix: jest config path

--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,7 @@ module.export = {
   transform: {
     '\\.(js|jsx)?$': 'babel-jest',
   },
-  testMatch: ['<rootDir>/**/>(*.)test.{js, jsx}'], // finds test
+  testMatch: ['<rootDir>/pages/**/>(*.)test.{js, jsx}'], // finds test
   moduleFileExtensions: ['js', 'jsx', 'json', 'node'],
   testPathIgnorePatterns: ['/node_modules/', '/.next/'],
 };
