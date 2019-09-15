8f6c3fb6dd7ed9da0c80f0da705bc267297eed42
Jakie W. Rice <jakie.rice@gmail.com>
2019-09-15 12:26:09 -0500

    refactor: change to hooks instead of Component API This is a great opportunity to try hooks in a manner I have not yet done. The useEffect hook is a great case for a subscription pattern.

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,26 +1,47 @@
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import io from 'socket.io-client';
 
-const URL = process.env.NODE_ENV === 'production' ? 'https://react-task.jakierice.now.sh/' : '';
-
-class Home extends React.Component {
-  state = {
-    hello: '',
-  };
-  static socket;
-  componentDidMount() {
-    console.log(URL);
-    this.socket = io();
-    this.socket.on('now', (data) => {
-      this.setState({
-        hello: data.value,
+function Home() {
+  const [currentRandomNumber, setCurrentRandomNumber] = useState({
+    value: 0,
+    timestamp: Number(new Date()),
+  });
+  const [numberList, setNumberList] = useState([]);
+  const { current: socket } = useRef(io());
+  useEffect(() => {
+    socket.on('random-number', data => {
+      setCurrentRandomNumber(data);
+      setNumberList(prevList => {
+        return prevList.concat(data);
       });
     });
-  }
 
-  render() {
-    return <div data-testid="home-root">{this.state.hello}</div>;
-  }
+    // the returned callback will be called by reacted
+    return () => {
+      socket.close();
+    };
+  }, []);
+
+  return (
+    <React.Fragment>
+      <header>
+        <h1>Entropy Party</h1>
+      </header>
+      <main>
+        <section>
+          <h2>Random Numbers</h2>
+          <button onClick={() => socket.close()}>Close connection</button>
+          <button onClick={() => socket.open()}>Open connection</button>
+          <strong>{currentRandomNumber.value}</strong>
+          <ul>
+            {numberList.map((number, index) => (
+              <li key={String(number.timestamp + index)}>{number.value}</li>
+            ))}
+          </ul>
+        </section>
+      </main>
+    </React.Fragment>
+  );
 }
 
 export default Home;
