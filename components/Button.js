6082484db55bb41b4dce38ab95156f36d9ef5345
Jakie W. Rice <jakie.rice@gmail.com>
2019-09-18 19:38:43 -0500

    styles(Button): adds basic button styles

---
+++ components/Button.js
@@ -0,0 +1,23 @@
+import styled from 'styled-components';
+
+export const Button = styled.button`
+  padding: 0.6rem 1.2rem;
+  font-size: 1rem;
+  background: none;
+  border: 1px solid
+    ${({ theme, disabled }) => (disabled ? theme.lightGray : theme.blue)};
+  color: ${({ theme, disabled }) => (disabled ? theme.lightGray : theme.blue)};
+  margin: 0 0.2rem 0.2rem 0;
+  transition: border-color 0.3s ease, color 0.3s ease;
+  cursor: pointer;
+
+  &:hover, &:focus {
+    outline: none;
+    border: 1px solid
+      ${({ theme, disabled }) => (disabled ? theme.lightGray : theme.yellow)};
+    color: ${({ theme, disabled }) =>
+      disabled ? theme.lightGray : theme.yellow};
+  }
+`;
+
+export const SecondayButton = styled.button``;
