951b7e541a33c64c7443c86ccd9f2e1e30dd2cc5
Jakie W. Rice <jakie.rice@gmail.com>
2019-09-18 19:39:27 -0500

    styles(Typography): adds basic text and copy styles

---
+++ components/Typography.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+export const PageTitle = styled.h1`
+  color: ${({ theme }) => theme.white};
+`;
+
+export const SectionTitle = styled.h2`
+  color: ${({ theme }) => theme.white};
+`;
+
+export const StrongText = styled.strong`
+  color: ${({ theme }) => theme.white};
+`;
+
+export const SmallText = styled.small`
+  color: ${({ theme }) => theme.white};
+`;
