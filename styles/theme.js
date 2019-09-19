5eca0e76bbd6001ee34529f6ef9089791baf04a3
Jakie W. Rice <jakie.rice@gmail.com>
2019-09-18 19:40:26 -0500

    styles(theme): adds color palette to new theme module

---
+++ styles/theme.js
@@ -0,0 +1,10 @@
+export default {
+  white: '#FFE8FF',
+  yellow: '#FFE74C',
+  darkGray: '#161416',
+  gray: '#353035',
+  lightGray: '#756A75',
+  pink: '#FF5964',
+  blue: '#2892D7',
+  red: '#D90368',
+};
