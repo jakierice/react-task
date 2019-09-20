4c16c34745190f69bb8b89acfe5a8d1e81c99f8d
Jakie W. Rice <jakie.rice@gmail.com>
2019-09-19 19:25:39 -0500

    refactor(Controls): moves chart controls into own component

---
+++ components/Controls.jsx
@@ -0,0 +1,69 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { ControlsLayoutWrapper, HorizontalRule } from '../styles/layout';
+import { ShowOnDesktopOnly } from '../styles/layout';
+import { Button } from './Button';
+import { FormLabel, RangeSlider, FormRowWrapper } from './Form';
+// import styled from 'styled-components';
+
+function Controls(props) {
+  const {
+    closeSocketConnection,
+    isSocketConnected,
+    openSocketConnection,
+    randomNumberAlertThreshold,
+    setRandomNumberAlertThreshold,
+    setSnapshotSize,
+    snapshotSize,
+  } = props;
+
+  return (
+    <ControlsLayoutWrapper>
+      <Button onClick={closeSocketConnection} disabled={!isSocketConnected}>
+        Close connection
+      </Button>
+      <Button onClick={openSocketConnection} disabled={isSocketConnected}>
+        Open connection
+      </Button>
+      <HorizontalRule />
+      <FormRowWrapper>
+        <FormLabel htmlFor="snapshot-size-slider">
+          Snapshot size: {snapshotSize}
+          <RangeSlider
+            type="range"
+            id="snapshot-size-slider"
+            name="snapshot-size-slider"
+            min="0"
+            max="30"
+            value={snapshotSize}
+            onChange={event => setSnapshotSize(event.target.value)}
+          />
+        </FormLabel>
+      </FormRowWrapper>
+      <FormLabel htmlFor="random-number-threshold-slider">
+        Alert threshold: {randomNumberAlertThreshold}
+        <RangeSlider
+          type="range"
+          id="random-number-threshold-slider"
+          name="random-number-threshold-slider"
+          min="0"
+          max="100"
+          value={randomNumberAlertThreshold}
+          onChange={event => setRandomNumberAlertThreshold(event.target.value)}
+        />
+      </FormLabel>
+    </ControlsLayoutWrapper>
+  );
+}
+
+Controls.propTypes = {
+  closeSocketConnection: PropTypes.bool,
+  isSocketConnected: PropTypes.bool,
+  openSocketConnection: PropTypes.func,
+  randomNumberAlertThreshold: PropTypes.number,
+  setRandomNumberAlertThreshold: PropTypes.func,
+  setSnapshotSize: PropTypes.func,
+  snapshotSize: PropTypes.number,
+};
+
+export default Controls;
