c63ecb021e475814b93016841ec0fd8925c57800
Jakie W. Rice <jakie.rice@gmail.com>
2019-09-20 12:58:27 -0500

    fix(testing): silence react deprecation console warnings in test

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -3,7 +3,18 @@ import { render, fireEvent, waitForElement } from '@testing-library/react';
 
 import '@testing-library/jest-dom/extend-expect';
 
-import Home from './index';
+import Home from '../pages/index';
+
+// mute console warnings caused by recharts library
+// using soon to be deprecated React methods
+const originalWarning = console.warn;
+beforeAll(() => {
+  console.warn = jest.fn();
+});
+
+afterAll(() => {
+  console.warn = originalWarning;
+});
 
 test('index page loads', () => {
   const { getByTestId } = render(<Home />);
